5d299356593bb0c57cac242ad5d00b25cba6fd29
sbossman <stigbosmans@gmail.com>
2018-01-01 20:32:34 +0100

    created proper functions

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import numpy as np
 def get_ema(current_price, last_ema=-1, number_of_days=26):
     if last_ema == -1:
         return current_price
     N = number_of_days
     k = 2/(N+1)
-    ema = (current_price * k) + (last_ema * (1-k))
+    ema = ((current_price - last_ema) * k) + (last_ema)
     return ema
+def calc_sma(prices, periods):
+    prices = np.array(prices)
+    sma = []
+    for i in range(periods,len(prices)-1):
+        sma.append(np.sum(prices[-periods:i]) / periods)
+    return sma
+def calc_ema(prices, periods):
+    sma = calc_sma(prices, periods)
+    ema = []
+    for i in range(0, len(sma) - 1):
+        if i == 0:
+            ema.append(sma[0])
+        else:
+            ema.append(get_ema(sma[i], ema[i - 1], periods))
+    return ema
+def calc_macd(prices):
+    ema_26 = calc_ema(prices, 26)
+    ema_12 = calc_ema(prices, 12)
+    macd_line = np.array(ema_12) - np.array(ema_26)
+    signal_line = calc_ema(macd_line,9)
+    return macd_line, signal_line
 
 iota_prices = pd.read_csv('data/iota_price.csv')
+iota_prices = iota_prices.iloc[::-1]
 close_prices = iota_prices["Close"].tolist()
-print(iota_prices)
-ema_26 = []
-ema_12 = []
-for i in range(0, len(close_prices) - 1):
-    last_price = 0
-    if i == 0:
-        ema_26.append(close_prices[i])
-        ema_12.append(close_prices[i])
-    else:
-        ema_26.append(get_ema(close_prices[i], ema_26[i-1], 26))
-        ema_12.append(get_ema(close_prices[i], ema_26[i - 1], 12))
-
-plt.plot(close_prices)
-plt.plot(ema_26)
-plt.plot(ema_12)
+macd, signal = calc_macd(close_prices)
+plt.plot(macd)
+plt.plot(signal)
 plt.show()
